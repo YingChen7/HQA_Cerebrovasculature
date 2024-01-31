c5fdf71e0fde8cbab98eedf202abc61e7fad017b
GShimmering <guobin420@gmail.com>
2024-01-31 11:48:06 +0800

    Add files via upload

--- 1_preprocessing/main_arrange_MRAfiles.m
+++ 1_preprocessing/main_arrange_MRAfiles.m
@@ -19,11 +19,21 @@ nc_make_sure_dir_exist(raw_mra_toseg);
 raw_mra_files=nc_generate_file_list(group_mra_dir, folder_prefix, ...
     file_prefix, file_suffix, '');
 
+iszipfile=0;
+if length(raw_mra_files) == 0
+    raw_mra_files=nc_generate_file_list(group_mra_dir, folder_prefix, ...
+        file_prefix, '.gz', '');
+    iszipfile=1;
+end
 % change each file to .gz format, which can be fed into deep learning
 % models
 for i=1:length(raw_mra_files)
     [fdir, fname, fext]=fileparts(raw_mra_files{i});
-    outfile=fullfile(raw_mra_toseg, [fname, fext, '.gz']);
+    if iszipfile == 0
+        outfile=fullfile(raw_mra_toseg, [fname, fext, '.gz']);
+    else
+        outfile=fullfile(raw_mra_toseg, [fname, fext]);
+    end
     save_untouch_nii(load_untouch_nii(raw_mra_files{i}), outfile);
 end
 
