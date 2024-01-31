736a192e08640721d5433d0af37297d00563edf8
GShimmering <guobin420@gmail.com>
2024-01-31 09:19:13 +0800

    Update nc_make_sure_dir_exist.m

--- utils/nc_make_sure_dir_exist.m
+++ utils/nc_make_sure_dir_exist.m
@@ -1,6 +1,15 @@
 function nc_make_sure_dir_exist(workdir)
-if dir_exist(workdir) == 1
+if nc_dir_exist(workdir) == 1
     return;
 else
     mkdir(workdir);
+end
+end
+
+function m=nc_dir_exist(path)
+    if exist(path, 'dir') == 7
+        m=1;
+    else
+        m=0;
+    end
 end
